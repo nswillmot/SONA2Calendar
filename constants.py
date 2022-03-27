69256ee4c4d70641b6b0819fc4072e85583ebc3d
Nick <89950098+nswillmot@users.noreply.github.com>
2022-03-27 12:42:12 +1000

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,29 +1,29 @@
 # URL of your SONA system
-sona_domain = 'https://uqpsych-p.sona-systems.com/'
+sona_domain = 'https://uqpsych-p.sona-systems.com/' # Amend to your school's SONA domain
 
 # A file that includes your SONA username (on the 1st line) and password (on the 2nd line)
-sona_credentials_file = 'sona_credentials.txt'
+sona_credentials_file = 'sona_credentials.txt' # Not needed if entered directly into sona2calendar.py file
 
 # Google calendar id (find it in your Calendar Settings -> Calendar ID)
 # Make sure the keys in this dictionary match the study names in the SONA system
-google_calendar_ids = {'Brain Stimulation Study; RST': 'qln020ujds51hs6erfm2qkvpog@group.calendar.google.com', 
-                       'Brain Stimulation Study; SRTT': '1phfhsormtqcdp6rhiofihve98@group.calendar.google.com'
+google_calendar_ids = {'My Very First Study': 'lhk678kj456k3j4klfb4@group.calendar.google.com', 
+                       'How much pain can an undergrad endure? $20!': 'lkh456lkad65q9v4nnm4@group.calendar.google.com'
                        }
 
 # This dictionary defines the study names to be displayed in the calendar, if different from the study names in SONA.
 # Example: {"A long and unclear study name being used in the SONA system": "Short Study Name"}
 # Change this to an empty dictionary if you want the study names to be same as in the SONA system
-calendar_study_names = {'Brain Stimulation Study; RST': 'RST',
-                        'Brain Stimulation Study; SRTT': 'SRTT'}
+calendar_study_names = {'My Very First Study': '1st',
+                        'How much pain can an undergrad endure? $20!': 'Pain'}
 
 # Color scheme for calendar events: if all words (separated by spaces) in a key string of this dictionary can be
 # found in the information of a study timeslot, the corresponding color (value in this dictionary) will be applied
 # to the calendar event of the study timeslot.
 # Example: {'Rm101 Alice': '1', 'Rm102 Alice': '2', 'Rm102 Bob': '3', 'Rm103': '4'}
 # For a reference of event colors see https://developers.google.com/google-apps/calendar/v3/reference/colors/get
-color_scheme = {'RST': '1', 'SRTT': '3', 'Hyon': '4', 'Gharibian': '5'}
+color_scheme = {'1st': '1', 'Pain': '3'}
 
 # If the researcher has a perferred name (other than their first name) to be shown on the calendar,
 # put it in this dictionary.
 # The key here should match the researcher name on SONA, the value is their preferred name. 
-researcher_names = {'Nicholas Wilmot': 'Nick'}
+researcher_names = {'Burrhus Frederic Skinner': 'B.F'}
